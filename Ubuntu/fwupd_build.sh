4c630f6c6cb30d5751b42d2421860f5ea3ba8d9d
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-04-03 15:36:26 +0900

    Create fwupd_build.sh for building fwupd
    Add a script to build and install fwupd

---
+++ Ubuntu/fwupd_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd fwupd
+
+meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu -Dplugin_modem_manager=enabled
+
+ninja -C build
+
+sudo ninja -C build install
+
+Check version:
+fwupdmgr --version
