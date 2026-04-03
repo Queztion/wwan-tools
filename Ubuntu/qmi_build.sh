64db3647769daa01e3d83b863851b671e5fe787b
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-04-03 15:32:11 +0900

    Update qmi_build.sh to remove cloning step
    Removed git clone command and changed directory.

--- Ubuntu/qmi_build.sh
+++ Ubuntu/qmi_build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-git clone https://gitlab.freedesktop.org/mobile-broadband/libqmi.git
-cd libqmi
 meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
 
 qmicli --version
