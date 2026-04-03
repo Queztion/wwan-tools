3799fa65aabe02657c43cae6b84418e992032fb4
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-04-03 15:35:53 +0900

    Clone fwupd repository and set up build environment

---
+++ Ubuntu/fwupd_clone_and_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/fwupd/fwupd.git
+cd fwupd
+
+meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu -Dplugin_modem_manager=enabled
+
+ninja -C build
+
+sudo ninja -C build install
+
+Check version:
+fwupdmgr --version
