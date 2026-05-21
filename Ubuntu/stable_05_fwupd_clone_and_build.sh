4df171af2cbb69acb1fc0e9b2c2a13cecb907a96
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-05-21 18:18:18 +0900

    Clone fwupd repository and checkout version 2.1.3

---
+++ Ubuntu/stable_05_fwupd_clone_and_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/fwupd/fwupd.git
+cd fwupd
+git checkout tags/2.1.3
+
+meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu -Dplugin_modem_manager=enabled -Ddocs=disabled
+
+ninja -C build
+
+sudo ninja -C build install
+
+# Check version:
+fwupdmgr --version
