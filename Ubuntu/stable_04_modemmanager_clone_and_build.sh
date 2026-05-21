4f9ae77891f486bfc6afc65df4593d042d513ed3
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-05-21 18:14:59 +0900

    Add checkout for ModemManager version 1.25.95-dev

---
+++ Ubuntu/stable_04_modemmanager_clone_and_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://gitlab.freedesktop.org/mobile-broadband/ModemManager.git
+cd ModemManager
+
+git checkout tags/1.25.95-dev
+
+meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu --sysconfdir=/etc
+
+mmcli --version
+
+ninja -C build
+sudo ninja -C build install
+
+mmcli --version
