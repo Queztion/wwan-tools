8741de9cd045e2d49b12a82698fd483ce9559cf2
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2026-05-29 17:02:17 +0900

    Fix libxmlb docs flag in build script

--- Ubuntu/stable_05_fwupd_clone_and_build.sh
+++ Ubuntu/stable_05_fwupd_clone_and_build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/fwupd/fwupd.git
 cd fwupd
 git checkout tags/2.1.3
 
-meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu -Dplugin_modem_manager=enabled -Ddocs=disabled -Dsubprojects/libxmlb:docs=disabled
+meson setup build --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu -Dplugin_modem_manager=enabled -Ddocs=disabled -Dlibxmlb:docs=disabled
 
 ninja -C build
 
